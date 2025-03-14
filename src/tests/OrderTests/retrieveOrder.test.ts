b652d56da819f87c224b800898907fe1df8b6551
Luke Fisher <Luke@student.unsw.edu.au>
2025-03-15 09:52:41 +1100

    added test

--- src/tests/OrderTests/retrieveOrder.test.ts
+++ src/tests/OrderTests/retrieveOrder.test.ts
@@ -4,37 +4,38 @@ import { jest } from '@jest/globals';
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 import { OrderRepository } from "../../repository/OrderRepository";
-import { createOrder, registerUserRequest } from "../testHelper"
+import { createOrder, registerUserRequest, retrieveOrder } from "../testHelper"
 import { PORT } from "../..";
 const SERVER_URL = `http://localhost:${PORT}`;
 
-const mockOderData = {
-    itemId: 'itemId',
-    itemQuantity: 2,
-    itemSeller: 'seller'
-}
+
 
 describe('retrieveOrder', () => {
     // let token: string;
     beforeEach(async () => {
-        // clear
+
 
     });
 
-    // test('successful order retrieval', async () => {
-    //     const token = (await registerUserRequest('uName', 'pWord', 'email@email.com')).data.token;
-    //     const order1 = await createOrder(
-    //         token, 
-    //         'personUId test', 
-    //         [mockOderData], 
-    //         'details'
-    //     );
+    test('successful order retrieval', async () => {
+        const mockOrderData = {
+            itemId: 'itemId',
+            itemQuantity: 2,
+            itemSeller: 'seller'
+        }
+        const token = (await registerUserRequest('uName', 'pWord', 'email@email.com')).data.token;
+        const order1 = await createOrder(
+            token, 
+            'personUId test', 
+            [mockOrderData], 
+            'details'
+        );
 
 
-    //     const result = await retrieveOrder(order1.data.result, token)
-    //     expect(result.statusCode).toStrictEqual(200);
-    //     expect(order1.data).toStrictEqual(mockOderData);
-    // });
+        const result = await retrieveOrder(order1.data.result, token)
+        expect(result.statusCode).toStrictEqual(200);
+        expect(order1.data).toStrictEqual(mockOrderData);
+    });
 
     test('successful order retrieval with hardcoded values', async () => {
         const orderUid = 'testOrderUid';
@@ -53,28 +54,18 @@ describe('retrieveOrder', () => {
         const result = await retrieveOrder(orderUid, token);
         expect(result).toStrictEqual(mockResponseData);
     });
-    // test('', async () => {
 
-    // });
-    // test('', async () => {
+    // test('order does not exist', async () => {
+    //     const orderUid = 'wrongOrderUid';
+    //     const token = 'testToken';
+    //     const mockResponseData = {
+    //         statusCode: 500,
+    //         data: 'error'
+    //     };
 
-    // });
-    // test('', async () => {
-
+    //     mockedAxios.get.mockRejectedValueOnce({ response: mockResponseData });
+    //     const result = await retrieveOrder(orderUid, token);
+    //     expect(result).toStrictEqual(mockResponseData);
+    
     // });
 });
-
-export async function retrieveOrder(orderUid: string, token: string) {
-    try {
-        const res = await axios.get(
-            `${SERVER_URL}/order/create/test/${orderUid}`,
-            {
-                headers: {token},
-                timeout: 5 * 1000
-            }
-        );
-        return res.data;
-    } catch (error) {
-       throw error;        
-    }
-}
