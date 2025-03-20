5ac9ff24ad74081e23517a8a24451d7f24c65c6f
AMadayag <hiding303@gmail.com>
2025-03-20 21:45:34 +1100

    added swagger ui files

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ const app: Application = express();
 })();
 
 // Serve Swagger UI using CDN
-const swaggerDocument = YAML.load(path.join(__dirname, 'public', 'swagger.yaml'));
+const swaggerDocument = YAML.load(path.join(__dirname, '../public', 'swagger.yaml'));
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Middleware
@@ -39,63 +39,11 @@ process.on('SIGINT', async () => {
   process.exit();
 });
 
+// Catch-all for error handling
+app.use((err: any, req: any, res: any, next: any) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
 // Export the Express app as a serverless function
 export default app;
-
-// import express, { Application } from 'express';
-// import * as swaggerUi from 'swagger-ui-express';
-// import * as path from 'path';
-// import * as YAML from 'yamljs';
-
-// import personRoutes from './routes/PersonRoutes';
-// import orderRoutes from './routes/OrderRoutes';
-// import cors from 'cors';
-// import { initDB, closeDB} from './database/DatabaseConnection';
-
-// (async () => {
-//   try {
-//     await initDB();
-//     console.log('Database initialized!');
-//   } catch (err) {
-//     console.error('Error initializing DB:', err);
-//   }
-// })();
-
-// // Load the swagger YAML file
-// const swaggerDocument = YAML.load(path.join(__dirname, 'swagger', 'swagger.yaml'));
-
-// // Optional: If you want to initialize a DB connection, import and call it:
-// // import { createDbConnection } from './database/DatabaseConnection';
-// // createDbConnection(); // Example usage
-
-// const app: Application = express();
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-
-// // Swagger docs
-// app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// // Routes
-// app.use('/api/person', personRoutes);
-// app.use('/api/order', orderRoutes);
-// //app.use('/api', adminRoutes);
-
-// // Start the server
-// const PORT = process.env.PORT || 3000;
-// const server = app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-// // For coverage, handle Ctrl+C gracefully
-// process.on('SIGINT', async () => {
-//   await server.close( async () => {
-//     console.log('Shutting down server gracefully.');
-//     await closeDB();
-//     process.exit();
-//   });
-// });
-
-// export { server }
-// export { PORT };
