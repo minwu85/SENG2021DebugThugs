908dcc0c322a43aa6a2775c7e24177c30ae28007
AMadayag <hiding303@gmail.com>
2025-04-08 13:44:08 +1000

    changed swagger server link

--- src/index.ts
+++ src/index.ts
@@ -42,7 +42,7 @@ const swaggerDocument = YAML.load(path.join(process.cwd(), 'public', 'swagger.ya
 // CDN CSS
 const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 // Serve Swagger UI at /api-docs
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
   customCss:
     '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
   customCssUrl: CSS_URL,
